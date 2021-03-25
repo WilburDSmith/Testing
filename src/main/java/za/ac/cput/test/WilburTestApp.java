e2ccabf9ed8c59d1de60ddeac6aebe029e892251
Alizwa Solibanzi <220001499@mycput.ac.za>
2021-03-25 18:15:25 +0200

    I added a  method to the app

--- src/main/java/za/ac/cput/test/WilburTestApp.java
+++ src/main/java/za/ac/cput/test/WilburTestApp.java
@@ -4,9 +4,15 @@ package za.ac.cput.test;
  *
  * @author Wilbur Smith
  * In my student number branch
- * 
+ * Added a method.
  */
 public class WilburTestApp {
    int a =5;
    int b = 6;
+  WilburTestApp (int a)
+  {
+  this.a= a ;   
+  
+  } 
+   
 }
